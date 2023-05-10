811e325f4352d27485d6edd77049611c3ef98e32
Md Mehrajul Islam <47929493+codermehraj@users.noreply.github.com>
2023-05-10 14:20:31 +0600

    Update Client.java

--- Creational Models/Builder/Client.java
+++ Creational Models/Builder/Client.java
@@ -3,9 +3,9 @@ public class Client {
         Burger burger = new Burger.BurgerBuilder()
                 .cheese(2).letus(1).cheese(1).meat(1).cheese(1).letus(1).cheese(1).build();
 
-        System.out.println(burger.numOfChese());
-        System.out.println(burger.numOfMeat());
-        System.out.println(burger.numOfLetus());
-        System.out.println(burger.getCost());
+        System.out.println("Chese : " + burger.numOfChese());
+        System.out.println("Meat : " + burger.numOfMeat());
+        System.out.println("Letus : " + burger.numOfLetus());
+        System.out.println("Cost : " + burger.getCost());
     }
 }
