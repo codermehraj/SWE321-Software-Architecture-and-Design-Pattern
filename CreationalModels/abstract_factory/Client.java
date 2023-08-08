e6fe15f904ee5c37efd4a2444a15b841a97625a9
codermehraj <codermehraj@gmail.com>
2023-08-08 23:04:31 +0600

    success fully added 1 extra new obstracle

--- CreationalModels/abstract_factory/Client.java
+++ CreationalModels/abstract_factory/Client.java
@@ -6,6 +6,8 @@ import CreationalModels.abstract_factory.concrete_factory.Asteriods.Level1Astero
 import CreationalModels.abstract_factory.concrete_factory.Asteriods.Level2AsteroidsFactory;
 import CreationalModels.abstract_factory.concrete_factory.DebrisField.Level1DebrisFieldFactory;
 import CreationalModels.abstract_factory.concrete_factory.DebrisField.Level2DebrisFieldFactory;
+import CreationalModels.abstract_factory.concrete_factory.SolarFlares.Level1SolarFlareFactory;
+import CreationalModels.abstract_factory.concrete_factory.SolarFlares.Level2SolarFlareFactory;
 import CreationalModels.abstract_factory.product.Obstacle;
 
 public class Client {
@@ -24,6 +26,12 @@ public class Client {
 
         if(level == 1) factory = new Level1DebrisFieldFactory();
         else factory = new Level2DebrisFieldFactory();
+
+        obstacle = factory.createObstacle(score);
+        obstacle.show();
+
+        if(level == 1) factory = new Level1SolarFlareFactory();
+        else factory = new Level2SolarFlareFactory();
 
         obstacle = factory.createObstacle(score);
         obstacle.show();
