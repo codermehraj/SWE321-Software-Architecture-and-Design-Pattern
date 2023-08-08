cf9e12ae94e2b879dd1537fbd876def96d59fd2e
codermehraj <codermehraj@gmail.com>
2023-08-08 23:13:43 +0600

    added rest 2 products

---
+++ CreationalModels/abstract_factory/product/BlackHoles/BlackHolesSmall.java
@@ -0,0 +1,10 @@
+package CreationalModels.abstract_factory.product.BlackHoles;
+
+public class BlackHolesSmall implements BlackHoles{
+
+    @Override
+    public void show() {
+        System.out.println("Small Black Hole Appeared");
+    }
+    
+}
---
+++ CreationalModels/abstract_factory/product/EnemyShips/EnemyShipsBlack.java
@@ -0,0 +1,10 @@
+package CreationalModels.abstract_factory.product.EnemyShips;
+
+public class EnemyShipsBlack implements EnemyShips{
+
+    @Override
+    public void show() {
+        System.out.println("Black Enemy Ship Appeared");
+    }
+    
+}
